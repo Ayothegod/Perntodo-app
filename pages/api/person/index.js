d52b322932a2f2117276902a41b2f4c2dfc913ff
Ayo <heyayomideadebisi@gmail.com>
2023-05-03 21:49:22 +0100

    create mock data

---
+++ pages/api/person/index.js
@@ -0,0 +1,21 @@
+
+const data = [
+    {
+        id:1,
+        name:"Lebron",
+        last_name:"james",
+        age:37,
+    },
+    {
+        id:2,
+        name:"lil",
+        last_name:"nas",
+        age:35,
+    },
+    {
+        id:3,
+        name:"rihanna",
+        last_name:"babe",
+        age:30,
+    },
+]
