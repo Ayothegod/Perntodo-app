07faf9459aa244295d81ebfbce786bee7ca99c87
Ayo <heyayomideadebisi@gmail.com>
2023-05-03 21:56:17 +0100

    code is ot coding, lets sleep

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,12 +2,7 @@
 
 
 export default function handler(req, res) {
-
-    if (req.method == "GET") {
-      res.status(200).json({ name: 'John Doe',age:30 , who:"boy"})
-  } else{
-
-  }
+  res.status(200).json({ name: 'John Doe',age:30 , who:"boy"})
 }
 
 
