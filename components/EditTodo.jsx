86f35033c999bf3d3368203bf1d404f69660340c
Ayo <heyayomideadebisi@gmail.com>
2023-05-05 13:12:48 +0100

    update worked bro!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- components/EditTodo.jsx
+++ components/EditTodo.jsx
@@ -2,31 +2,35 @@ import { useState } from "react"
 
 const EditTodo = ({modal, setModal,content,id}) => {
     console.log(id);
-    const [modalContent,setModalContent] = useState(content)
+    const [description,setDescription] = useState(content)
 
     const updateTodo = async() => {
         // e.preventDefault()
         try {
-            const body = {modalContent}
+            const body = {description}
             const response = await fetch(`http://localhost:3000/api/users/${id}`,{
                 method:"PUT",
                 headers:{
                     "Content-Type":"application/json"
                 },
-                body: JSON.stringify(modalContent)
+                body: JSON.stringify(body)
             })
+            window.location = "/"
             console.log(response);
         } catch (error) {
             console.log(error);
         }
     }
 
+    // localhost/api/users/55 
+    // http://localhost:3000/api/users/54
+
     return (
         <div className="fixed inset-0 w-screen bg-[rgba(0,0,0,0.5)] h-screen flex items-center justify-center">
             <div className="w-full sm:w-2/3 md:1/2 bg-white shadow-xl rounded-md p-4">
                 <p className="text-lg font-semibold text-purple-700">Edit Todo</p>
                 <div>
-                    <textarea name="" cols="30" rows="5" className="w-full border border-neutral-400 rounded" value={modalContent} onChange={(e) => setModalContent(e.target.value)}>
+                    <textarea name="" cols="30" rows="5" className="w-full border border-neutral-400 rounded" value={description} onChange={(e) => setDescription(e.target.value)}>
                     </textarea>
                 </div>
                 <div className="text-right space-x-4 text-sm">
