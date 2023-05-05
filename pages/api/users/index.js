64afc0d17a331d27da9362af3012c510175c3628
Ayo <heyayomideadebisi@gmail.com>
2023-05-05 14:25:29 +0100

    dadd next corss, maybe itll work

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -4,10 +4,9 @@ import NextCors from 'nextjs-cors';
 
 export default async function handler(req, res) {
   await NextCors(req, res, {
-    // Options
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200,
  });
 
   if(req.method == "GET"){
