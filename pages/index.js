a9ba9942543e9190e2a825628723f3f0e3bad334
Ayo <heyayomideadebisi@gmail.com>
2023-05-19 23:33:14 +0100

    swr stuff

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,6 @@ export default function Home() {
   // const { data, error, isLoading } = useSWR("http://localhost:3000/api/users", fetcher)
   // // console.log(data);
 
-  // if (error) return <div>failed to load</div>
-  // if (isLoading) return <div>loading...</div>
   return (
     <>
       <Head>
